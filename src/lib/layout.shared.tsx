2678f24c255de571e202d1568ba9accacc4cc350
politokk <133917774+politokk@users.noreply.github.com>
2025-08-25 03:41:02 -0400

    metajson

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -14,14 +14,14 @@ export function baseOptions(): BaseLayoutProps {
         <>
           <div
             style={{
-              width: "20px",
-              height: "20px",
+              width: "22px",
+              height: "22px",
               borderRadius: "50%",
               background: "linear-gradient(90deg, hsla(154, 100%, 76%, 1) 0%, hsla(234, 100%, 83%, 1) 50%, hsla(288, 100%, 81%, 1) 100%)",
             }}
             aria-label="Logo"
           />
-          AIUI
+          <span className="text-xl tracking-tight text-subtle-foreground font-semibold">AIUI</span>
         </>
       ),
     },
